494a1be6ae2820c8379d4573b7320315602e17f9
premdass <premdass.r@gmail.com>
2020-08-05 13:44:12 +0100

    Initial Commit with an Abstract class as guide on the methods to implement

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # JavaTest
-Java Coding Test for the JazzNetworks Cloud Enginner Position for Premdaas
+Java Coding Test for the JazzNetworks Cloud Engineer Position for Premdaas
+
+
+# Question 1
+##### Implement (from scratch, without using Java library components) a doubly linked list (of integers) class which allows new integers to be added to either end of the list, any referenced node to be removed, and the first and last integers to be read quickly
---
+++ src/main/java/com/jazz/interview/AbstractSequenceList.java
@@ -0,0 +1,32 @@
+package com.jazz.interview;
+
+abstract class AbstractSequenceList {
+
+    public abstract void add(int index, int item);
+
+    public abstract void addFirst(int item);
+
+    public abstract void addLast(int item);
+
+    public abstract void add(int item);
+
+    public abstract int getFirst();
+
+    public abstract int getLast();
+
+    public abstract int get(int index);
+
+    public abstract int removeFirst();
+
+    public abstract int removeLast();
+
+    public abstract int remove(int index);
+
+    public abstract int removeFirstOccurrence(int item);
+
+    public abstract int set(int index, int item);
+
+    //TODO: size and toArray
+
+
+}
